76ded3084e48456b1c68c1176461597ccab14f43
sonar <sonaraklpesh1998@gmail.com>
2026-03-28 23:16:25 +0530

    Initial commit

---
+++ src/main/java/com/api/auth/ApiauthApplication.java
@@ -0,0 +1,13 @@
+package com.api.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiauthApplication.class, args);
+	}
+
+}
